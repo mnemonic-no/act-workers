738c1f0691b07db6b2668893fa951f557ae8fb91
Fredrik Borg <fredrik.borg@gmail.com>
2019-03-14 13:44:58 +0100

    scio-worker: support new data model and add tests (#21)
    * support new data model and add tests
    
    * add tests
    
    * add extraction of uri and ipv4Network
    
    * remove unused import
    
    * add uri components

--- scio_worker.py
+++ scio_worker.py
@@ -2,25 +2,36 @@
 
 """NiFi worker to pass Scio produced data to the ACT platform"""
 
-import sys
 import argparse
 import json
-from logging import error
+import sys
 import traceback
+from logging import error
 
 import act
+from act.helpers import handle_fact, handle_uri
 
 EXTRACT_GEONAMES = ["countries", "regions", "regions-derived",
                     "sub-regions", "sub-regions-derived"]
 
+
 EXTRACT_INDICATORS = ["md5", "sha1", "sha256",
                       "fqdn", "ipv4", "ipv6", "email",
-                      "msid", "cve"]
+                      "msid", "cve", "uri", "ipv4net"]
+
+SCIO_GEONAMES_ACT_MAP = {
+    "countries": "country",
+    "regions": "region",
+    "regions-derived": "region",
+    "sub-regions": "subRegion",
+    "sub-regions-derived": "subRegion"
+}
 
 SCIO_INDICATOR_ACT_MAP = {
     "md5": "hash",
     "sha1": "hash",
     "sha256": "hash",
+    "ipv4net": "ipv4Network",
     "cve": "vulnerability",
     "msid": "vulnerability",
 }
@@ -44,13 +55,14 @@ def get_scio_report():
     return json.load(sys.stdin)
 
 
-def seen_in_report_fact(actapi, object_type, object_values, report_id):
+def report_mentions_fact(actapi, object_type, object_values, report_id):
     for value in list(set(object_values)):
         try:
-            actapi.fact("seenIn", "report")\
-                .source(object_type, value)\
-                .destination("report", report_id)\
-                .add()
+            handle_fact(
+                actapi.fact("mentions", object_type)
+                .source("report", report_id)
+                .destination(object_type, value)
+            )
         except act.base.ResponseError as e:
             error("Unable to create linked fact: %s" % e)
 
@@ -64,9 +76,10 @@ def add_to_act(actapi, doc):
 
     try:
         # Report title
-        actapi.fact("hasTitle", title)\
-            .source("report", report_id)\
-            .add()
+        handle_fact(
+            actapi.fact("name", title)
+            .source("report", report_id)
+        )
     except act.base.ResponseError as e:
         error("Unable to create fact: %s" % e)
 
@@ -77,38 +90,49 @@ def add_to_act(actapi, doc):
         # Get object type from ACT (default to object type in SCIO)
         act_indicator_type = SCIO_INDICATOR_ACT_MAP.get(scio_indicator_type,
                                                         scio_indicator_type)
-        seen_in_report_fact(
+        report_mentions_fact(
             actapi,
             act_indicator_type,
             indicators.get(scio_indicator_type, []),
             report_id)
 
+    # For SHA256, create content object
+    for sha256 in list(set(indicators.get("sha256", []))):
+        handle_fact(
+            actapi.fact("represents")
+            .source("hash", sha256)
+            .destination("content", sha256))
+
+    # Add all URI components
+    for uri in list(set(indicators.get("uri", []))):
+        handle_uri(actapi, uri)
+
     # Locations (countries, regions, sub regions)
     for location_type in EXTRACT_GEONAMES:
         locations = doc.get("geonames", {}).get(location_type, [])
 
-        seen_in_report_fact(
+        report_mentions_fact(
             actapi,
-            "location",
+            SCIO_GEONAMES_ACT_MAP[location_type],
             locations,
             report_id)
 
     # Threat actor
-    seen_in_report_fact(
+    report_mentions_fact(
         actapi,
         "threatActor",
         doc.get("threat-actor", {}).get("names", []),
         report_id)
 
     # Tools
-    seen_in_report_fact(
+    report_mentions_fact(
         actapi,
         "tool",
-        doc.get("tools", {}).get("names", []),
+        [tool.lower() for tool in doc.get("tools", {}).get("names", [])],
         report_id)
 
     # Sector
-    seen_in_report_fact(
+    report_mentions_fact(
         actapi,
         "sector",
         doc.get("sectors", []),
@@ -119,9 +143,11 @@ if __name__ == '__main__':
     ARGS = parseargs()
 
     try:
-        actapi = act.Act(ARGS.act_baseurl, ARGS.act_user_id, ARGS.loglevel, ARGS.logfile, "scio")
         # Add IOCs from reports to the ACT platform
-        add_to_act(actapi, get_scio_report())
+        add_to_act(
+            act.Act(ARGS.act_baseurl, ARGS.act_user_id, ARGS.loglevel, ARGS.logfile, "scio"),
+            get_scio_report()
+        )
     except Exception as e:
         error("Unhandled exception: {}".format(traceback.format_exc()))
         raise
---
+++ test/test_scio.py
@@ -0,0 +1,42 @@
+""" Test for scio worker """
+import json
+
+import act
+import scio_worker
+
+
+def test_scio_facts(capsys) -> None:  # type: ignore
+    """ Test for scio facts, by comparing to captue of stdout """
+    with open("test/scio-doc.json") as scio_doc:
+        doc = json.loads(scio_doc.read())
+
+    api = act.Act("", None, "error")
+
+    scio_worker.add_to_act(api, doc)
+
+    captured = capsys.readouterr()
+    facts = set(captured.out.split("\n"))
+
+    report_id = doc["hexdigest"]
+
+    sha256 = doc["indicators"]["sha256"][0]
+    uri = doc["indicators"]["uri"][0] # "http://www.us-cert.gov/tlp."
+
+    fact_assertions = [
+        api.fact("name", "TA18-149A.stix.xml").source("report", report_id),
+        api.fact("mentions", "ipv4").source("report", report_id).destination("ipv4", "187.127.112.60"),
+        api.fact("mentions", "hash").source("report", report_id).destination("hash", "4613f51087f01715bf9132c704aea2c2"),
+        api.fact("mentions", "hash").source("report", report_id).destination("hash", sha256),
+        api.fact("mentions", "country").source("report", report_id).destination("country", "Colombia"),
+        api.fact("mentions", "uri").source("report", report_id).destination("uri", uri),
+        api.fact("componentOf").source("fqdn", "www.us-cert.gov").destination("uri", uri),
+        api.fact("componentOf").source("path", "/tlp.").destination("uri", uri),
+        api.fact("scheme", "http").source("uri", uri),
+        api.fact("mentions", "tool").source("report", report_id).destination("tool", "kore"),
+        api.fact("mentions", "email").source("report", report_id).destination("email", "redhat@gmail.com"),
+        api.fact("mentions", "ipv4Network").source("report", report_id).destination("ipv4Network", "192.168.0.0/16"),
+        api.fact("represents").source("hash", sha256).destination("content", sha256)
+    ]
+
+    for fact_assertion in fact_assertions:
+        assert str(fact_assertion) in facts
