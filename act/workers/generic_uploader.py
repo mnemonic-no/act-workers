4e70bab2c119fa9496b72f8f9f1d4827682e4363
Fredrik Borg <fredrik.borg@gmail.com>
2020-10-30 13:57:14 +0100

    add timing for handle_fact in uploader (#116)
    * add timing
    
    * remove round() of len()
    
    Co-authored-by: Fredrik Borg <fredrikb@mnemonic.no>

--- act/workers/generic_uploader.py
+++ act/workers/generic_uploader.py
@@ -3,31 +3,61 @@
 """General ACT backend uploader. Reads facts as JSON
 from the stdin, uploading accordingly"""
 
+import argparse
 import json
 import sys
+import time
 import traceback
 from logging import error, warning
 
 import act.api
+
 from act.workers.libs import worker
 
 
-def main(actapi: act.api.Act) -> None:
+def parseargs() -> argparse.ArgumentParser:
+    """ Parse arguments """
+    parser = worker.parseargs('Generic uploader')
+    parser.add_argument('--timing', action="store_true",
+                        help="Add timing operations at warn level")
+
+    return parser
+
+
+def main(actapi: act.api.Act, timing: bool = False) -> None:
     """Process stdin, parse each separat line as a JSON structure and
     register a fact based on the structure. The form of input should
     be the on the form accepted by the ACT Rest API fact API."""
+
+    handle_fact_time = []
 
     for line in sys.stdin:
         data = json.loads(line)
 
         fact = actapi.fact(**data)
         try:
+            started = time.time()
             act.api.helpers.handle_fact(fact)
+
+            time_spent = time.time() - started
+            handle_fact_time.append(time_spent)
+            if timing:
+                warning("Handle fact time: %s", round(time_spent, 2))
         except act.api.base.ValidationError as err:
             warning("ValidationError while storing objects: %s" % err)
         except act.api.base.ResponseError as err:
             error("ResponseError while storing objects: %s" % err)
             sys.exit(1)
+
+    if timing:
+        warning(
+            "Total time (count:%s,total:%s,mean:%s,min:%s,max:%s)",
+            len(handle_fact_time),
+            round(sum(handle_fact_time), 2),
+            round(sum(handle_fact_time)/len(handle_fact_time), 2),
+            round(min(handle_fact_time), 2),
+            round(max(handle_fact_time), 2),
+        )
 
 
 def main_log_error() -> None:
@@ -35,10 +65,10 @@ def main_log_error() -> None:
     try:
         # Look for default ini file in "/etc/actworkers.ini" and ~/config/actworkers/actworkers.ini
         # (or replace .config with $XDG_CONFIG_DIR if set)
-        args = worker.handle_args(worker.parseargs("Generic uploader"))
+        args = worker.handle_args(parseargs())
         actapi = worker.init_act(args)
 
-        main(actapi)
+        main(actapi, args.timing)
     except Exception:
         error("Unhandled exception: {}".format(traceback.format_exc()))
         raise
